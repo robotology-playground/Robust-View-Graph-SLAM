b31b9d83fca3f65d3dc30d830e7a9d8b0a361c2d
Tariq <t.abuhashim@gmail.com>
2016-12-23 13:15:52 +1300

    moved mex to matlab

--- mex/compile_source.m
+++
@@ -1,6 +0,0 @@
-%--------------------------------------------------------------------------
-function compile_source(d, include, output, mex_src, libs)
-fprintf(['Compiling ' output '\n']);
-s = sprintf ('mex %s -DDLONG -O %s -output %s %s', d, include, output, mex_src);
-s = [s ' ' libs];
-eval(s);
--- mex/getversion.m
+++
@@ -1,5 +0,0 @@
-%--------------------------------------------------------------------------
-function v = getversion
-% determine the MATLAB version, and return it as a double.
-v = sscanf(version, '%d.%d.%d');
-v = 10.^(0:-1:-(length(v)-1)) * v;
